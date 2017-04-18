1d7f51fe0865818095d14ec73087887dd23965d9
Kunal Botla <kunalbotla@gmail.com>
2017-04-18 19:17:33 -0400

    First Commit

---
+++ Pickup/Pickup/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pickup
+//
+//  Created by Kunal Botla on 4/18/17.
+//  Copyright © 2017 Kunal Botla. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
